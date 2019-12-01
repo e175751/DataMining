e296c750a8625121a410f5a2361af0e7c95262d0
e175751@ie.u-ryukyu.ac.jp <e175751 e175751@ie.u-ryukyu.ac.jp>
2019-12-02 00:33:45 +0900

    new data

---
+++ work/report2/LR.py
@@ -0,0 +1,12 @@
+import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+def main():
+    lr = LogisticRegression()
+        
+
+
+if __name__ == "__main__":
+    main()
+
